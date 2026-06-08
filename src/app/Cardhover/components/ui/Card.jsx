7ddc622622fc7f84e9bb8ba8a874c83c63dbb7d6
Bored008 <dahiyahimanshu80@gmail.com>
2026-06-08 16:53:18 +0530

    added blur

--- src/app/Cardhover/components/ui/Card.jsx
+++ src/app/Cardhover/components/ui/Card.jsx
@@ -22,7 +22,21 @@ const Card = () => {
       <div className='bg-gray-100 flex-1 mt-4 rounded-lg border border-dashed border-neutral-200 relative'>
         
         {/* motion div */}
-        <motion.div className='absolute inset-0 h-full w-full bg-white rounded-lg divide-y divide-neutral-200'>
+        <motion.div 
+        initial={{
+          opacity:0,
+          scale:0.98,
+          filter:"blur(10px)"
+        }}
+        whileHover={{
+          opacity:1,
+          scale:1.05,
+          filter:"blur(0px)",
+        }}
+        transition={{
+          duration:0.5
+        }}
+        className='absolute inset-0 h-full w-full bg-white rounded-lg divide-y divide-neutral-200'>
           <div className='p-4 flex gap-2'>
             <div className='h-7 w-7 flex-shrink-0 bg-gradient-to-r shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)]
           bg-white rounded-md flex items-center justify-center'>
