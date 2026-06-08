38a158cb5e4fea8742fdafea45b64f39ffa65f7c
Bored008 <dahiyahimanshu80@gmail.com>
2026-06-08 20:53:04 +0530

    added spring animation

--- src/app/Cardhover/components/ui/Card.jsx
+++ src/app/Cardhover/components/ui/Card.jsx
@@ -34,9 +34,11 @@ const Card = () => {
           filter:"blur(0px)",
         }}
         transition={{
-          duration:0.5
+          type:"spring",
+          stiffness:100,
+          damping:15
         }}
-        className='absolute inset-0 h-full w-full bg-white rounded-lg divide-y divide-neutral-200'>
+        className='absolute inset-0 h-full border border-neutral-200 w-full bg-white rounded-lg divide-y divide-neutral-200'>
           <div className='p-4 flex gap-2'>
             <div className='h-7 w-7 flex-shrink-0 bg-gradient-to-r shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)]
           bg-white rounded-md flex items-center justify-center'>
