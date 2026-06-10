33a6d106b1a2672ee2a1d2bb444b3382f0a88b1a
Bored008 <dahiyahimanshu80@gmail.com>
2026-06-10 13:25:46 +0530

    learned color change animation

--- src/app/MotionHook/components/Motionhooks.tsx
+++ src/app/MotionHook/components/Motionhooks.tsx
@@ -1,69 +1,110 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { IconRocket } from "@tabler/icons-react";
 import Image from "next/image";
 import { div } from "motion/react-client";
-import { motion, useMotionTemplate, useMotionValueEvent, useScroll, useSpring, useTransform } from "motion/react";
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValueEvent,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "motion/react";
 
 const Motionhooks = () => {
+  const [bgColor, setBgColor] = useState("#232323");
+
+  const backgrounds = ["#232323", "#926c15", "#14452f"];
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-neutral-900 text-white">
-      <div className="mx-auto max-w-4xl flex flex-col gap-10 py-40">
-        {features.map((feature,idx)=>(
-          <Card key={feature.title} feature={feature} />
+    <motion.div
+      style={{
+        backgroundColor: bgColor,
+      }}
+      className="flex min-h-screen items-center justify-center bg-neutral-900 text-white"
+    >
+      <div className="mx-auto flex max-w-4xl flex-col gap-10 py-40">
+        {features.map((feature, idx) => (
+          <Card
+            key={feature.title}
+            feature={feature}
+            bg={backgrounds[idx]}
+            setBgColor={setBgColor}
+          />
         ))}
       </div>
+    </motion.div>
+  );
+};
+
+const Card = ({
+  feature,
+  bg,
+  setBgColor,
+}: {
+  feature: Feature;
+  bg: string;
+  setBgColor: any;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  const translateContent = useSpring(
+    useTransform(scrollYProgress, [0, 1], [200, -300]),
+    {
+      stiffness: 100,
+      damping: 30,
+      mass: 1,
+    },
+  );
+  const opacityContent = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0]);
+  const blur = useTransform(scrollYProgress, [0, 0.3, 0.5, 0.8, 1], [10, 5, 0, 5, 10]);
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    if (latest > 0.4 && latest < 0.6) {
+      setBgColor(bg);
+    }
+    console.log("changed value", latest);
+  });
+
+  return (
+    <div
+      ref={ref}
+      key={feature.title}
+      className="items-cen grid grid-cols-2 gap-20 py-80"
+    >
+      <motion.div
+        style={{
+          filter: useMotionTemplate`blur(${blur}px)`,
+        }}
+        className="flex flex-col gap-5"
+      >
+        {feature.icon}
+        <h2 className="text-4xl font-bold">{feature.title}</h2>
+        <p className="text-lg text-neutral-400">{feature.description}</p>
+      </motion.div>
+      <motion.div
+        style={{
+          y: translateContent,
+          opacity: opacityContent,
+        }}
+      >
+        {feature.content}
+      </motion.div>
     </div>
   );
 };
 
-const Card = ({feature}:{feature: Feature})=>{
-  const ref = useRef<HTMLDivElement>(null);
-  const {scrollYProgress} = useScroll({
-    target:ref,
-    offset:["start end","end start"]
-  });
-
-  const translateContent = useSpring(useTransform(scrollYProgress,[0,1],[200,-300]),{
-    stiffness:100,
-    damping:30,
-    mass:1
-  });
-  const opacityContent = useTransform(scrollYProgress,[0,0.5,1],[0,1,0]);
-  const blur = useTransform(scrollYProgress,[0,0.5,1],[10,0,10]);
-
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    console.log("changed value", latest);
-  });
-
-  return(
-  <div 
-  ref={ref}
-  key={feature.title} className="grid grid-cols-2 items-cen gap-20 py-80">
-      <motion.div 
-      style={{
-        filter:useMotionTemplate`blur(${blur}px)`
-      }}
-      className="flex flex-col gap-5">
-        {feature.icon}
-        <h2 className="text-4xl font-bold">{feature.title}</h2>
-        <p className="text-neutral-400 text-lg">{feature.description}</p>
-      </motion.div>
-    <motion.div
-    style={{
-      y: translateContent, 
-      opacity: opacityContent,
-    }}>{feature.content}</motion.div>
-  </div>
-  )
-};
-
 type Feature = {
-  icon:React.ReactNode;
-  title:string;
-  description:string;
-  content:React.ReactNode;
-}
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+  content: React.ReactNode;
+};
 
 const features = [
   {
@@ -114,6 +155,5 @@ const features = [
     ),
   },
 ];
-
 
 export default Motionhooks;
